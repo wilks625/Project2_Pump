a5de50545fa82bd912c720d6b39e86155852c3a1
Stephen Yacas <syacas123@gmail.com>
2021-06-02 17:40:47 -0400

    added get routes for profile and user api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const profileRoutes = require('./profileRoutes');
+const matchRoutes = require('./userRoutes')
 
 router.use('/users', userRoutes);
 router.use('/profiles', profileRoutes);
+
+router.use('/matches', matchRoutes )
 
 module.exports = router;
--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,2 @@
-DROP DATABASE IF EXISTS crowdfund_db;
-CREATE DATABASE crowdfund_db;
+DROP DATABASE IF EXISTS pumpUsers_db;
+CREATE DATABASE pumpUsers_db;
