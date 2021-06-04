711fe565c719449f7751d21baf4fb3701f2efca6
Stephen Yacas <syacas123@gmail.com>
2021-06-03 20:13:29 -0400

    working on put request

--- controllers/api/profileRoutes.js
+++ controllers/api/profileRoutes.js
@@ -31,37 +31,57 @@ router.get('/:id', async (req, res) => {
   }
 })
 
-// router.post('/', withAuth, async (req, res) => {
-//   try {
-//     const newProfile = await Profile.create({
-//       ...req.body,
-//       user_id: req.session.user_id,
-//     });
+router.post('/',/* withAuth ,*/ async (req, res) => {
+  try {
+    const newProfile = await Profile.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
 
-//     res.status(200).json(newProfile);
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
+    res.status(200).json(newProfile);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-// router.delete('/:id', withAuth, async (req, res) => {
-//   try {
-//     const profileData = await Profile.destroy({
-//       where: {
-//         id: req.params.id,
-//         user_id: req.session.user_id,
-//       },
-//     });
+router.delete('/:id', /* withAuth ,*/ async (req, res) => {
+  try {
+    const profileData = await Profile.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
 
-//     if (!profileData) {
-//       res.status(404).json({ message: 'No profile found with this id!' });
-//       return;
-//     }
+    if (!profileData) {
+      res.status(404).json({ message: 'No profile found with this id!' });
+      return;
+    }
 
-//     res.status(200).json(profileData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.status(200).json(profileData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  console.log(req.body)
+  console.log(req.params)
+  try {
+    const userData = await User.update(
+      { 
+        location: req.body.location ,
+       returning: true
+      },
+      {where: {id: parseInt(req.params.id)} }
+    )
+    console.log(userData)
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+
+});
 
 module.exports = router;
