4cbe1759273e3096c17465ff1ceef40542cd75c5
Stephen Yacas <syacas123@gmail.com>
2021-06-05 14:39:53 -0400

    added login and display userprofile functionality

--- public/js/login.js
+++ public/js/login.js
@@ -25,14 +25,14 @@
 // Testing Below
 const loginFormHandler = async (event) => {
   event.preventDefault();
-
+console.log("stephen")
   // Collect values from the login form
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
     // Send a POST request to the API endpoint
-    const response = await fetch('/api/users/', {
+    const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
