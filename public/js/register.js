ad0501a6e87f328936ee06e132d09d62fa12605e
Stephen Yacas <syacas123@gmail.com>
2021-06-06 22:10:41 -0400

    added create user and profile functionality

--- public/js/register.js
+++ public/js/register.js
@@ -2,24 +2,31 @@ const signupFormHandler = async (event) => {
     event.preventDefault();
     console.log("stephen")
   
-    const fname = document.querySelector('#firstname-signup').value.trim();
-    const lname = document.querySelector('#lastname-signup').value.trim();
+    const firstname = document.querySelector('#firstname-signup').value.trim();
+    const lastname = document.querySelector('#lastname-signup').value.trim();
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (fname && lname && username && email && password) {
-      const response = await fetch('/api/users/register', {
+    if (firstname && lastname && username && email && password) {
+      const response = await fetch('/api/users/', {
         method: 'POST',
-        body: JSON.stringify({ fname, lname, username, email, password }),
+        body: JSON.stringify({ firstname, lastname, username, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
           console.log(response)
+          const profile = await fetch('/api/profiles/', {
+            method: 'POST',
+            body: JSON.stringify({ id: response.id }),
+            headers: { 'Content-Type': 'application/json' },
+          });
+
         document.location.replace('/userprofile');
       } else {
         alert(response.statusText);
+        console.log(response)
       }
     }
   };
