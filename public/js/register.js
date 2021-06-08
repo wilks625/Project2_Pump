df375e8b0559e98e9cf71f544603264b0a419fae
Stephen Yacas <syacas123@gmail.com>
2021-06-07 20:36:29 -0400

    cleaned nav bar and commented out uneeded code

--- public/js/register.js
+++ public/js/register.js
@@ -1,36 +1,37 @@
 const signupFormHandler = async (event) => {
-    event.preventDefault();
-    console.log("stephen")
-  
-    const firstname = document.querySelector('#firstname-signup').value.trim();
-    const lastname = document.querySelector('#lastname-signup').value.trim();
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (firstname && lastname && username && email && password) {
-      const response = await fetch('/api/users/', {
+  event.preventDefault();
+  console.log('stephen');
+
+  const firstname = document.querySelector('#firstname-signup').value.trim();
+  const lastname = document.querySelector('#lastname-signup').value.trim();
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (firstname && lastname && username && email && password) {
+    const response = await fetch('/api/users/', {
+      method: 'POST',
+      body: JSON.stringify({ firstname, lastname, username, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      console.log(response);
+      const profile = await fetch('/api/profiles/', {
         method: 'POST',
-        body: JSON.stringify({ firstname, lastname, username, email, password }),
+        body: JSON.stringify({ id: response.id }),
         headers: { 'Content-Type': 'application/json' },
       });
-  
-      if (response.ok) {
-          console.log(response)
-          const profile = await fetch('/api/profiles/', {
-            method: 'POST',
-            body: JSON.stringify({ id: response.id }),
-            headers: { 'Content-Type': 'application/json' },
-          });
 
-        document.location.replace('/userprofile');
-      } else {
-        alert(response.statusText);
-        console.log(response)
-      }
+      document.location.replace('/userprofile');
+    } else {
+      alert(response.statusText);
+      console.log(response);
+      console.log('stephenERROR');
     }
-  };
+  }
+};
 
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
